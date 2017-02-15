e2e34fe8f015838c26a998add612cccbbc9293c7
Justine Cocchi <jucocchi@microsoft.com>
2017-02-15 15:25:20 -0800

    added parse results code snippet

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,16 +26,19 @@ namespace WebApplication.Controllers
             var submittedPhoto = link;
 
             // Make API Call and get back top emotion
-            //var topEmotion = await MakeRequest(submittedPhoto);
+            var results = await MakeRequest(submittedPhoto);
+
+            // Parse the results and get the top emotion
+            var topEmotion = ParseResults(results);
 
             // Decide which picture and description to display
-            //var suggestion = MakeSuggestion(topEmotion);
+            var suggestion = MakeSuggestion(topEmotion);
 
             // Pass the suggestion to the Results page to be displayed
-            //return RedirectToAction("Results", suggestion);
+            return RedirectToAction("Results", suggestion);
 
             // Remove this once you paste in the proper code snippets
-            return View();
+            //return View();
         }
 
         [HttpGet]
@@ -58,7 +61,109 @@ namespace WebApplication.Controllers
         // =========================================================
         // Helper functions, paste code snippets here
         // =========================================================
+public async Task<String> MakeRequest(string submittedPhoto)
+{
+    var client = new HttpClient();
 
+    // Add request headers
+    client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "96cc520d6c1c4856919b4c6d4640086a"); // TODO: Fix key
+    var uri = "https://api.projectoxford.ai/emotion/v1.0/recognize";
+            
+    // Format request body
+    String picURL= "\"" + submittedPhoto + "\"";
+    String format = "\"url\":";
+    String requestBody= String.Format("{0} {1}", format, picURL);
+    requestBody = "{" + requestBody + "}";
+    byte[] byteData = Encoding.UTF8.GetBytes(requestBody);
+
+    // Send request
+    HttpResponseMessage response;
+    using (var content = new ByteArrayContent(byteData))
+    {
+        content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+        response = await client.PostAsync(uri, content).ConfigureAwait(false);
+    }
+    
+    // Convert JSON results to a string to be parsed later           
+    HttpContent httpResults = response.Content;
+    var results = await httpResults.ReadAsStringAsync();
+
+    return results;
+}
+
+public string ParseResults(string results)
+{
+    // Set up objects to store top emotion
+    EmotionSet emotions = new EmotionSet();            
+    string topEmotion = "";
+
+    // Trim unneccesary brackets from the response 
+    results = results.TrimStart('[');
+    results= results.TrimEnd(']');
+
+    // Validate response: an invalid link will return an error code and an invalid image will return "[]"            
+    if (!results.Contains("error") && results.Length > 2)
+    {
+        // Populate a JSON object with the results of the API call and find the top emotion
+        JsonConvert.PopulateObject(results, emotions);
+        topEmotion = emotions.getTopScore();
+    }
+    
+    return topEmotion;
+}
+
+public Result MakeSuggestion(string emotion)
+{
+    var link = "";
+    var description = "";
+    // Decide which image to use based on the emotion in the picture
+    switch (emotion)
+    {
+        case "anger":
+            link = "/images/AngryDwight.jpg";
+            description = "You look just as angry as Dwight!";
+            break;
+        case "contempt":
+            link = "/images/Angela.jpg";
+            description = "Your contempt level is reaching Angela levels!";
+            break;
+        case "disgust":
+            link = "/images/Kelly.gif";
+            description = "You look just as disgusted as Kelly!";
+            break;
+        case "fear":
+            link = "/images/Michael.jpg";
+            description = "You might just be just as fearful and superstitous as Michael!";
+            break;
+        case "happiness":
+            link = "/images/JimAndPam.png";
+            description = "You look just as happy as Jim and Pam together!";
+            break;
+        case "neutral":
+            link = "/images/Stanley.jpg";
+            description = "Looking just as neutral as Stanley staring at the camera.";
+            break;
+        case "sadness":
+            link = "/images/SadDwight.jpg";
+            description = "Looking as sad as Dwight today!";
+            break;
+        case "surprise":
+            link = "/images/Jim.jpg";
+            description = "You look just as suprised as Jim walking in on Dwight's birthday suprise.";
+            break;
+        case "":
+            link = "/images/Error.jpg";
+            description = "Oops something went wrong! Please make sure that you submitted the correct image link and that your face is both promienent in the image and unobstructed. Submit another link to try again!";
+            break;
+    }
+    // Store suggestion
+    Result suggestion = new Result()
+    {
+        photo = link,
+        description = description
+    };
+    return suggestion;
+}
 
 
         // =========================================================
--- Models/EmotionSet.cs
+++ Models/EmotionSet.cs
@@ -23,10 +23,53 @@ namespace WebApplication.Models
         public double sadness { get; set; }
         public double surprise { get; set; }
 
-        // Replace this function with the Max() function from the TopEmotion code snippet
-        public string Max(){
-            return null;
+        // // Replace this function with the Max() function from the TopEmotion code snippet
+        // public string Max(){
+        //     return null;
+        // }
+
+        public string Max()
+{
+    // Store all emotions for easier comparison
+    double[] scores = new double[] { anger,
+                                   contempt,
+                                   disgust,
+                                   fear,
+                                   happiness,
+                                   neutral,
+                                   sadness,
+                                   surprise };
+    // Find top emotion
+    int topEmotion = 0;
+    for (int i = 0; i < scores.Length; i++)
+    {
+        if (scores[i] > scores[topEmotion])
+        {
+            topEmotion = i;
         }
+    }
+    // Return top emotion
+    switch (topEmotion)
+    {
+        case 0:
+            return "anger";
+        case 1:
+            return "contempt";
+        case 2:
+            return "disgust";
+        case 3:
+            return "fear";
+        case 4:
+            return "happiness";
+        case 5:
+            return "neutral";
+        case 6:
+            return "sadness";
+        case 7:
+            return "surprise";
+    }
+    return null;
+}
     }
 
     [JsonObject]
