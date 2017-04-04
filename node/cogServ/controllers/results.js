6309734156192c156185912d2544335e4a818b07
Justine Cocchi <jucocchi@microsoft.com>
2017-04-04 14:47:04 -0700

    factored out code snippets

--- node/cogServ/controllers/results.js
+++ node/cogServ/controllers/results.js
@@ -1,6 +1,4 @@
 const request = require('request-promise-native')
-const parseEmotions = require('../models/emotion.js')
-
 const express = require('express')
 const router = express.Router()
 
@@ -10,21 +8,29 @@ var EMOTION_URL = 'https://westus.api.cognitive.microsoft.com/emotion/v1.0/recog
 router.post('/', function (req, res, next) {
   var url = req.body.url
 
-  // MAKE CALL TO COG SERVICES
   Promise.all([
-    callAPI(url)
-  ]).then(([results]) => {
+    // MAKE CALL TO COG SERVICES
+    // callAPI(url)
+  ]).then(([response]) => {
+    var results = response
+
     // PARSE RESULTS HERE
-    var topEmotion = parseResponse(results)
+    // var topEmotion = parseResponse(results)
 
     // DETERMINE WHICH IMAGE TO DISPLAY
-    var suggestion = makeSuggestion(topEmotion)
+    // var suggestion = makeSuggestion(topEmotion)
 
     // RENDER RESULTS
-    res.render('results',
-      { title: 'Results',
-        description: suggestion.description,
-        photo: suggestion.photo })
+    // res.render('results',
+    //   { title: 'Results',
+    //     description: suggestion.description,
+    //     photo: suggestion.photo })
+
+    // DELETE ME AFTER UNCOMMENTING ABOVE
+    res.render('results', {
+      title: 'Results',
+      description: 'You entered: ' + url
+    })
   }).catch(reason => {
     console.log('Promise was rejected becasue ' + reason)
 
@@ -42,100 +48,3 @@ module.exports = router
 // =========================================================
 // HELPER FUNCTIONS HERE
 // =========================================================
-function callAPI (url) {
- // Call API
-  return request.post({
-    url: EMOTION_URL,
-    json: {'url': url}
-  }).then((result) => {
-    return result
-  })
-}
-
-function parseResponse (response) {
-  // Create array of emotion values
-  var emotions = parseEmotions(response[0].scores)
-
-  // Find top emotion
-  var topEmotion = 0
-  for (var i = 0; i < 8; i++) {
-    if (emotions[i] > emotions[topEmotion]) {
-      topEmotion = i
-    }
-  }
-
-  // Return top emotion
-  switch (topEmotion) {
-    case 0:
-      return 'anger'
-    case 1:
-      return 'contempt'
-    case 2:
-      return 'disgust'
-    case 3:
-      return 'fear'
-    case 4:
-      return 'happiness'
-    case 5:
-      return 'neutral'
-    case 6:
-      return 'sadness'
-    case 7:
-      return 'surprise'
-  }
-  return null
-}
-
-function makeSuggestion (topEmotion) {
-  var link = ''
-  var description = ''
-
-  // Decide which image to use based on the emotion passed in
-  switch (topEmotion) {
-    case 'anger':
-      link = '/images/AngryDwight.jpg'
-      description = 'You look just as angry as Dwight!'
-      break
-    case 'contempt':
-      link = '/images/Angela.jpg'
-      description = 'Your contempt level is reaching Angela levels!'
-      break
-    case 'disgust':
-      link = '/images/Kelly.gif'
-      description = 'You look just as disgusted as Kelly!'
-      break
-    case 'fear':
-      link = '/images/Michael.jpg'
-      description = 'You might just be just as fearful and superstitous as Michael!'
-      break
-    case 'happiness':
-      link = '/images/JimAndPam.png'
-      description = 'You look just as happy as Jim and Pam together!'
-      break
-    case 'neutral':
-      link = '/images/Stanley.jpg'
-      description = 'Looking just as neutral as Stanley staring at the camera.'
-      break
-    case 'sadness':
-      link = '/images/SadDwight.jpg'
-      description = 'Looking as sad as Dwight today!'
-      break
-    case 'surprise':
-      link = '/images/Jim.jpg'
-      description = "You look just as suprised as Jim walking in on Dwight's birthday suprise."
-      break
-    case '':
-      link = '/images/Error.jpg'
-      description = 'Oops something went wrong! Please make sure that you submitted the correct image link and that your face is both promienent in the image and unobstructed. Submit another link to try again!'
-      break
-  }
-
-  // Store suggestion
-  var suggestion = {
-    photo: link,
-    description: description
-  }
-
-  return suggestion
-}
-
